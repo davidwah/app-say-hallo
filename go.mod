29821aad1d92c65178c7ed9393a2b2d6422bb516
davidwah <davidwahyuyu@gmail.com>
2022-02-23 13:26:51 +0700

    add app say-hallo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/davidwah/app-say-hallo
+
+go 1.17
+
+require github.com/davidwah/go-say-hallo v1.5.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_say_hallo "github.com/davidwah/go-say-hallo"
+)
+
+func main() {
+	fmt.Println(go_say_hallo.SayHallo())
+}
