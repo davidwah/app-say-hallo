f1aaac38b2dd3c225a0d2c3ded365a1c2dac154b
davidwah <davidwahyuyu@gmail.com>
2022-02-23 15:21:17 +0700

    update app say-hallo/v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/davidwah/app-say-hallo
 
 go 1.17
 
-require github.com/davidwah/go-say-hallo v1.5.0
+require github.com/davidwah/go-say-hallo/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	go_say_hallo "github.com/davidwah/go-say-hallo"
+	go_say_hallo "github.com/davidwah/go-say-hallo/v2"
 )
 
 func main() {
-	fmt.Println(go_say_hallo.SayHallo())
+	fmt.Println(go_say_hallo.SayHallo("DWP"))
 }
